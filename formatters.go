e2218e3c7ed77ec68afd448ef76775e352c04303
Josh Mervine <joshua@mervine.net>
2015-09-24 18:07:11 -0700

    fixing kv pair formatter with quoting

--- formatters.go
+++ formatters.go
@@ -5,47 +5,29 @@ import (
 	"strings"
 )
 
-// KeyValue is the default formatter, and formats like so:
+// KeyValue formats like so:
 //
-//  Log("package", "server", "listern", ":3000")
-//  //=> 2015/08/21 20:01:48 package=server listener=:3000
+//  readable.Log("package name", "server", "listern", ":3000", "extra stuff")
+//  //=> 2015/08/21 20:01:48 "package name"=server listener=:3000 "extra stuff"
 //
 func KeyValue(parts ...interface{}) (line string) {
 
 	segments := []string{}
 
-	for i := 0; i < len(parts); i++ {
-		if i%2 == 0 {
-			if i == len(parts)-1 {
-				segments = append(segments, fmt.Sprintf("%+v", parts[i]))
-			} else {
-				segments = append(segments, fmt.Sprintf("%+v=%+v", parts[i], parts[i+1]))
-			}
+	quote := func(i interface{}) string {
+		s := fmt.Sprintf("%+v", i)
+		if strings.Contains(s, " ") {
+			return fmt.Sprintf("%q", s)
 		}
-
+		return s
 	}
 
-	return strings.Join(segments, " ")
-}
-
-// KeyValueQuoted formats like so:
-//
-//  readable.SetFormatter(readable.KeyValueQuoted)
-//  readable.Log("package", "server", "listern", ":3000", "extra stuff")
-//  //=> 2015/08/21 20:01:48 package="server" listener=":3000" "extra stuff"
-//
-func KeyValueQuoted(parts ...interface{}) (line string) {
-
-	segments := []string{}
-
 	for i := 0; i < len(parts); i++ {
 		if i%2 == 0 {
 			if i == len(parts)-1 {
-				val := fmt.Sprintf("%+v", parts[i])
-				segments = append(segments, fmt.Sprintf("%q", val))
+				segments = append(segments, quote(parts[i]))
 			} else {
-				val := fmt.Sprintf("%+v", parts[i+1])
-				segments = append(segments, fmt.Sprintf("%+v=%q", parts[i], val))
+				segments = append(segments, fmt.Sprintf("%s=%s", quote(parts[i]), quote(parts[i+1])))
 			}
 		}
 
--- formatters_test.go
+++ formatters_test.go
@@ -14,48 +14,25 @@ type TestStruct struct {
 
 func TestReadable_KeyValue(T *testing.T) {
 	str := KeyValue("foo", "bar", 1)
-	exp := "foo=bar 1"
+	exp := `foo=bar 1`
 	Go(T).AssertEqual(str, exp)
 
 	str = KeyValue("foo", "bar", true)
-	exp = "foo=bar true"
+	exp = `foo=bar true`
 	Go(T).AssertEqual(str, exp)
 
-	str = KeyValue("foo", "bar", "args", []string{"foo", "bar"})
-	exp = "foo=bar args=[foo bar]"
+	str = KeyValue("foo", "bar", "my args", []string{"foo", "bar"})
+	exp = `foo=bar "my args"="[foo bar]"`
 	Go(T).AssertEqual(str, exp)
 
-	str = KeyValue("foo", "bar", "struct", TestStruct{"foo", 9, false})
-	exp = "foo=bar struct={A:foo B:9 C:false}"
+	str = KeyValue("foo", "bar bin", "struct", TestStruct{"foo", 9, false})
+	exp = `foo="bar bin" struct="{A:foo B:9 C:false}"`
 	Go(T).AssertEqual(str, exp)
 
 	// weird one
 	obj := TestStruct{"foo", 9, false}
 	str = KeyValue(obj, obj)
-	exp = "{A:foo B:9 C:false}={A:foo B:9 C:false}"
-}
-
-func TestReadable_KeyValueQuoted(T *testing.T) {
-	str := KeyValueQuoted("foo", "bar", 1)
-	exp := `foo="bar" "1"`
-	Go(T).AssertEqual(str, exp)
-
-	str = KeyValueQuoted("foo", "bar", true)
-	exp = `foo="bar" "true"`
-	Go(T).AssertEqual(str, exp)
-
-	str = KeyValueQuoted("foo", "bar", "args", []string{"foo", "bar"})
-	exp = `foo="bar" args="[foo bar]"`
-	Go(T).AssertEqual(str, exp)
-
-	str = KeyValueQuoted("foo", "bar", "struct", TestStruct{"foo", 9, false})
-	exp = `foo="bar" struct="{A:foo B:9 C:false}"`
-	Go(T).AssertEqual(str, exp)
-
-	// weird one
-	obj := TestStruct{"foo", 9, false}
-	str = KeyValueQuoted(obj, obj)
-	exp = `{A:foo B:9 C:false}="{A:foo B:9 C:false}"`
+	exp = `"{A:foo B:9 C:false}"="{A:foo B:9 C:false}"`
 }
 
 func TestReadable_Join(T *testing.T) {
